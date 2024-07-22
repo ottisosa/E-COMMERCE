f50478fbb158b31e40e5b4bf9803752b2c2a3d32
ottisosa <ottisosa10@gmail.com>
2024-07-21 22:21:35 -0300

    inicio de modelado de la BD

--- APP/CORE/connection.php
+++ APP/CORE/connection.php
@@ -1,36 +1,41 @@
 <?php
 
-class dataBaseConn{
+require_once "config.php";
 
-
-
-  //  Funcion para la conexion de la base de datos  //
-
-    function connection()
-    {
-        $host = "localhost";
-        $username = "root";
-        $password = "";
-        $db = "";
-        $port = 3306;
-        $conn = new mysqli($host, $username, $password, $db, $port);
-
-
-
-        // Para verificar la conexion //
-
-        if ($conn->connect_error) {
-            die("Failed connection: ".$conn->connect_error);
-        } else {
-            echo "Succesful connection !";
+class Database {
+    
+    // Objeto de conexion MySQLi
+    private $mysqli;
+    
+    //Instancia unica de la clase Database
+    private static $instanceDB;
+    
+    // Metodo para establecer conexion a la base de datos
+    public function connect(){
+        
+        //Crear una nueva conexion MySQLi
+        $this->mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT);
+        
+        // Verificar si existen errores en la conexion
+        if ($this->mysqli->connect_error){
+            // Lanzar una excepcion
+            throw new Exception ('Error de conexion: ' . $this->mysqli->connect_error);
         }
-
-        return $conn;
+    }
+    
+    // Metodo estatico para obtencion de la instancia de la clase Database
+    public static function getInstanceDB(){
+        
+        //Verificar si la instancia NO existe
+        if (!self::$instanceDB){
+            // Crear nueva instancia de Database
+            self::$instanceDB = new Database();
+            //Establecer la conexion llamando al metodo connect()
+            self::$instanceDB->connect();
+        }
+        //Retorna el bojeto mysqli para su uso
+        return self::$instanceDB->mysqli;
     }
 }
-
-
-
-
 
 ?>
